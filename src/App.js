f6f0a5047abf0278caf528c194e4d4457d7a0cf4
Edgard Henrique <edigarhenriqu@gmail.com>
2025-09-09 19:58:18 -0300

    Calculadora

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import Input from './components/Input';
+import Button from './components/Button';
+
+import { Container, Content, Row } from "./styles";
+import { useState } from 'react';
+
+const App = () => {
+  const [currentNumber, setCurrentNumber] = useState('0');
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState(null);
+
+  const handleOnClear = () => {
+    setCurrentNumber('0');
+    setFirstNumber('0');
+    setOperation(null);
+  };
+
+  const handleAddNumber = (num) => {
+    setCurrentNumber(prev => `${prev === "0" ? "" : prev}${num}`);
+  };
+
+  const handleOperation = (op) => {
+    if (firstNumber === '0') {
+      setFirstNumber(currentNumber);
+      setCurrentNumber('0');
+      setOperation(op);
+    } else {
+      handleEquals();
+      setOperation(op);
+    }
+  };
+
+  const handleEquals = () => {
+    if (operation && firstNumber !== '0' && currentNumber !== '0') {
+      let result = 0;
+      switch (operation) {
+        case '+':
+          result = Number(firstNumber) + Number(currentNumber);
+          break;
+        case '-':
+          result = Number(firstNumber) - Number(currentNumber);
+          break;
+        case 'x':
+          result = Number(firstNumber) * Number(currentNumber);
+          break;
+        case '/':
+          result = Number(firstNumber) / Number(currentNumber);
+          break;
+        default:
+          return;
+      }
+      setCurrentNumber(String(result));
+      setFirstNumber('0');
+      setOperation(null);
+    }
+  };
+
+  return (
+    <Container>
+      <Content>
+        <Input value={currentNumber}/>
+        <Row>
+          <Button label="x" onClick={() => handleOperation("x")} />
+          <Button label="/" onClick={() => handleOperation("/")} />
+          <Button label="C" onClick={handleOnClear}/>
+          <Button label="-" onClick={() => handleOperation("-")} />
+        </Row>
+        <Row>
+          <Button label="7" onClick={() => handleAddNumber("7")}/>
+          <Button label="8" onClick={() => handleAddNumber("8")}/>
+          <Button label="9" onClick={() => handleAddNumber("9")}/>
+          <Button label="+" onClick={() => handleOperation("+")} />
+        </Row>
+        <Row>
+          <Button label="4" onClick={() => handleAddNumber("4")}/>
+          <Button label="5" onClick={() => handleAddNumber("5")}/>
+          <Button label="6" onClick={() => handleAddNumber("6")}/>
+          <Button label="=" onClick={handleEquals}/>
+        </Row>
+        <Row>
+          <Button label="1" onClick={() => handleAddNumber("1")}/>
+          <Button label="2" onClick={() => handleAddNumber("2")}/>
+          <Button label="3" onClick={() => handleAddNumber("3")}/>
+          <Button label="0" onClick={() => handleAddNumber("0")}/>
+        </Row>
+      </Content>
+    </Container>
+  );
+};
+
+export default App;
